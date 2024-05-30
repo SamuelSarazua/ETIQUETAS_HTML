2d9e1860f4be9ffd0b027e1e8ff99b6d4151e6d2
jerónimo <sajeronimo@scl.edu.gt>
2024-05-30 16:34:59 -0600

    seccion3 de jeronimo

--- section3/section3.js
+++ section3/section3.js
@@ -1,3 +1,47 @@
 let sec3_dom = document.querySelector(".section3");
 
-sec3_dom.innerHTML = `Hola soy seccion 3`;
+sec3_dom.innerHTML = `
+<h1>Formulario</h1>
+<table border="">
+    <tr>
+        <td>form</td>
+        <td>Define el inicio y el final del formulario.</td>
+    </tr>
+    <tr>
+        <td>label</td>
+        <td>Define una etiqueta para un elemento de formulario.</td>
+    </tr>
+    <tr>
+        <td>input</td>
+        <td>Define un campo de entrada.</td>
+    </tr>
+    <tr>
+        <td>textarea</td>
+        <td>Define un área de texto multilinea.</td>
+    </tr>
+    <tr>
+        <td>select y option</td>
+        <td>Define una lista desplegable.</td>
+    </tr>
+    <tr>
+        <td>button</td>
+        <td>Define un botón.</td>
+    </tr>
+    <tr>
+        <td>fieldset y legend</td>
+        <td>Agrupan elementos relacionados dentro de un formulario.</td>
+    </tr>
+    <tr>
+        <td>input type="radio"</td>
+        <td>Define un botón de opción.</td>
+    </tr>
+    <tr>
+        <td>input type="checkbox"</td>
+        <td>Define una casilla de verificación.</td>
+    </tr>
+    <tr>
+        <td>input type="date"</td>
+        <td>Define un selector de fecha.</td>
+    </tr>
+</table>
+`;
