6119a7b380234e966b6e0fba92fc720ac76fde6c
SamuelSarazua <sesarazua@scl.edu.gt>
2024-05-30 16:49:19 -0600

    Actualizacion de Header y Footer

--- footer/footer.js
+++ footer/footer.js
@@ -3,4 +3,7 @@ let footer_dom = document.querySelector(".footer");
 footer_dom.innerHTML = `
     <h1 class="scl">SCL</h1>
     <h2 class="arr">@sesarazua</h2>
+    <h2 class="arr">@bemorales</h2>
+    <h2 class="arr">@sajeronimo</h2>
+    <h2 class="arr">@hdvillatoro</h2>
 `;
